2d2f832dabd37f5aba94010a7340966e4f87d012
Marijus <marijus.matulevicius@gmail.com>
2021-08-10 15:06:42 +0300

    tests and POM created

---
+++ pages/calculatorPage.js
@@ -0,0 +1,82 @@
+const buildFieldSelector = 'select[name="selectBuild"]';
+const operationFieldSelector = 'select[name="selectOperation"]';
+const inputOneSelector = 'input[name="number1"]';
+const inputTwoSelector = 'input[name="number2"]';
+const calculateButtonSelector = 'input:has-text("Calculate")';
+const answerFieldSelector = '#numberAnswerField';
+
+const addValue = '0';
+const substractValue = '1';
+const multiplyValue = '2';
+const divideValue = '3';
+const concatenateValue = '4';
+
+exports.CalculatorPage = class CalculatorPage {
+  constructor(page) {
+    this.page = page;
+  }
+
+  async goto() {
+    await this.page.goto('https://testsheepnz.github.io/BasicCalculator');
+  }
+
+  async selectBuild(buildVersion) {
+    this.page.selectOption(buildFieldSelector, buildVersion);
+  }
+
+  async fillInputFields(inputOne, inputTwo) {
+    await this.page.fill(inputOneSelector, inputOne);
+    await this.page.fill(inputTwoSelector, inputTwo);
+  }
+
+  async add(inputOne, inputTwo) {
+    await this.page.selectOption(operationFieldSelector, addValue);
+    await this.fillInputFields(inputOne, inputTwo)
+    await this.page.click(calculateButtonSelector);
+    const result = await this.page.inputValue(answerFieldSelector);
+    return result;
+  }
+
+  async substract(inputOne, inputTwo) {
+    await this.page.selectOption(operationFieldSelector, substractValue);
+    await this.fillInputFields(inputOne, inputTwo)
+    await this.page.click(calculateButtonSelector);
+    const result = await this.page.inputValue(answerFieldSelector);
+    return result;
+  }
+
+  async multiply(inputOne, inputTwo) {
+    await this.page.selectOption(operationFieldSelector, multiplyValue);
+    await this.fillInputFields(inputOne, inputTwo)
+    await this.page.click(calculateButtonSelector);
+    const result = await this.page.inputValue(answerFieldSelector);
+    return result;
+  }
+
+  async divide(inputOne, inputTwo) {
+    await this.page.selectOption(operationFieldSelector, divideValue);
+    await this.fillInputFields(inputOne, inputTwo)
+    await this.page.click(calculateButtonSelector);
+    const result = await this.page.inputValue(answerFieldSelector);
+    return result;
+  }
+
+  async concatenate(inputOne, inputTwo) {
+    await this.page.selectOption(operationFieldSelector, concatenateValue);
+    await this.fillInputFields(inputOne, inputTwo)
+    await this.page.click(calculateButtonSelector);
+    const result = await this.page.inputValue(answerFieldSelector);
+    return result;
+  }
+
+  async reqElementsVisible() {
+    const buildField = await this.page.isVisible(buildFieldSelector);
+    const operationField = await this.page.isVisible(operationFieldSelector);
+    const inputOne = await this.page.isVisible(inputOneSelector);
+    const inputTwo = await this.page.isVisible(inputTwoSelector);
+    const calculateButton = await this.page.isVisible(calculateButtonSelector);
+    const answerField = await this.page.isVisible(answerFieldSelector);
+    const allElements = [buildField, operationField, inputOne, inputTwo, calculateButton, answerField];
+    return allElements.every(Boolean);
+  }
+};
---
+++ tests/calculator-homework.js
@@ -0,0 +1,56 @@
+const { test, expect } = require('@playwright/test');
+const { CalculatorPage } = require('../pages/calculatorPage');
+
+const inputOne = '6';
+const inputTwo = '3';
+
+const addRes = `${Number(inputOne) + Number(inputTwo)}`;
+const subRes = `${Number(inputOne) - Number(inputTwo)}`;
+const mulRes = `${Number(inputOne) * Number(inputTwo)}`;
+const divRes = `${Number(inputOne) / Number(inputTwo)}`;
+const conRes = inputOne + inputTwo;
+
+const buildVersions = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+buildVersions.forEach(buildVersion => {
+  test.describe(`Testing build No. ${buildVersion}`, () => {
+    let page;
+
+    test.beforeAll(async ({ browser }) => {
+      page = await browser.newPage();
+      calculatorPage = new CalculatorPage(page);
+    });
+
+    test.beforeEach(async () => {
+      await calculatorPage.goto();
+      await calculatorPage.selectBuild(buildVersion);
+      const reqElementsVisible = await calculatorPage.reqElementsVisible();
+      expect(reqElementsVisible).toBe(true);
+    });
+
+    test('addition is working properly', async () => {
+      const result = await calculatorPage.add(inputOne, inputTwo);
+      expect(result).toEqual(addRes);
+    });
+
+    test('substraction is working properly', async () => {
+      const result = await calculatorPage.substract(inputOne, inputTwo);
+      expect(result).toEqual(subRes);
+    });
+
+    test('multiplication is working properly', async () => {
+      const result = await calculatorPage.multiply(inputOne, inputTwo);
+      expect(result).toEqual(mulRes);
+    });
+
+    test('division is working properly', async () => {
+      const result = await calculatorPage.divide(inputOne, inputTwo);
+      expect(result).toEqual(divRes);
+    });
+
+    test('concatenation is working properly', async () => {
+      const result = await calculatorPage.concatenate(inputOne, inputTwo);
+      expect(result).toEqual(conRes);
+    });
+
+  });
+});
